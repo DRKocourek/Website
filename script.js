fb4a5f5f3f6925aa2effd2af1f0a56190bad405f
DRKocourek <4DRKocourek@gmail.com>
2024-08-13 10:31:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let logo = "github";
+let picture = "";
+const el = document.querySelector("img");
+const pic = document.getElementById("picture");
 function show(){
     const element=document.getElementById("text");
     element.innerHTML = "Meow"
@@ -6,4 +10,57 @@ function print(){
     const parelement = document.querySelector("p");
     let print = document.getElementById("text").value;
     parelement.innerHTML = print;
+}
+function next() {
+
+  if (logo === "github") {
+    el.src="img/github.svg";
+    logo = "youtube";
+  }
+  else if (logo === "youtube") {
+      el.src="img/youtube.svg";
+      logo = "instagram"
+  }
+  else if (logo === "instagram") {
+      el.src="img/instagram.png";
+      logo = "tiktok"
+  }
+  else if (logo === "tiktok") {
+      el.src="img/tiktok.png";
+      logo = "github"
+  }
+}
+function prev() {
+  if (logo === "github") {
+    el.src="img/github.svg";
+    logo = "tiktok";
+  }
+  else if (logo === "youtube") {
+      el.src="img/youtube.svg";
+      logo = "github"
+  }
+  else if (logo === "instagram") {
+      el.src="img/instagram.png";
+      logo = "youtube"
+  }
+  else if (logo === "tiktok") {
+      el.src="img/tiktok.png";
+      logo = "instagram"
+  }
+}
+function showpic() {
+  var e = document.getElementById("test");
+  var value = e.value;
+  if (value==="js") {
+    pic.src = "img/js.png"
+  }
+  else if (value==="idk") {
+    pic.src = "img/boykisser.png"
+  }
+  else if (value==="rickroll") {
+    window.open("https://youtu.be/dQw4w9WgXcQ?si=toqOp3HBRVml6q-0", "_blank");
+  }
+}
+function rickroll() {
+      window.open("https://youtu.be/dQw4w9WgXcQ?si=toqOp3HBRVml6q-0", "_blank");
 }
